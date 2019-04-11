57e6b0eaebe926dbfd6caf28bfc9c8bdd3ead75d
Mateus Guedes <teuguedes@outlook.com>
2019-04-11 00:27:53 -0300

    each pokemon is appear dinamically

--- index.js
+++ index.js
@@ -25,49 +25,42 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 
-function addPokemon(data) {
+async function addPokemon(data) {
 
   for( pokemon in data.results) {
-    console.log(data.results[pokemon])
+    //console.log(data.results[pokemon].url)
     const section = document.getElementById("summary")
+    
     const divPokemonBlock = document.createElement("div")
-    divPokemonBlock.className = "pokemon-block"
+    divPokemonBlock.className += "pokemon-block"
+
+    const divPokemon = document.createElement("div")
+    divPokemon.className += "pokemon"
+    
+    const pokemonName = document.createElement("div")
+    pokemonName.className += "pokemon-name"
+
+    const paragraphName = document.createElement("p")
+    paragraphName.append(`${data.results[pokemon].name}`);
+
+    const pokemonImage = document.createElement("div")
+    pokemonImage.className += "pokemon-image"
+
+    const image = document.createElement("img")
+    image.src = await getPokemonImg(data.results[pokemon].url)
+    
+    pokemonName.append(paragraphName)
+    pokemonImage.append(image)
+    divPokemon.append(pokemonImage)
+    divPokemon.append(pokemonName)
+    divPokemonBlock.append(divPokemon)
     section.append(divPokemonBlock)
   }
 
-  // data.results.map( (pokemon, index) => {
-  //   createBlock(pokemon)
-  // })
 }
 
-function createBlock(pokemon) {
-    console.log(pokemon)
-    const section = document.getElementById("summary")
-    
-
-
-    const divPokemonBlock = document.createElement("div")
-    divPokemonBlock.className = "pokemon-block"
-
-    // const divPokemon = document.createElement("div")
-    // divPokemon.className += "pokemon"
-
-    // const pokemonImage = document.createElement("div")
-    // pokemonImage += "pokemon-image"
-
-    // const image = document.createElement("img")
-    // image.src += 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/25.png'
-
-    // const pokemonName = document.createElement("div")
-    // pokemonName.className += "pokemon-name"
-
-    // const name = document.createElement("span")
-    // name.textContent = pokemon.name
-
-    // pokemonName.append(name)
-    // pokemonImage.append(image)
-    // divPokemon.append(pokemonImage)
-    // divPokemon.append(pokemonName)
-    // divPokemonBlock.append(divPokemon)
-    section.append(divPokemonBlock)
+async function getPokemonImg(pokemonUri) {
+  const response = await fetch(`${pokemonUri}`)
+  const res = await response.json()
+  return res.sprites.front_default
 }
